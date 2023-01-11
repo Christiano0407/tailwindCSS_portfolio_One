1fa330312aee4bba72bdf741b738ebf6154860e9
Christiano0407 <placebo0704@gmail.com>
2023-01-11 12:34:49 -0600

    finished all javascript process

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,27 @@
 //** === === Javascript Portfolio With Tailwind CSS === ===  */
+//**TODO === === Events === ===  */
+
+const updateList = () => {
+  const titles = [...document.querySelectorAll(`h1, h2`)].sort((a, b) => {
+    return (
+      Math.abs(a.getBoundingClientRect().top) -
+      Math.abs(b.getBoundingClientRect().top)
+    );
+  });
+
+  document.querySelectorAll(`.selected-circle`).forEach((element) => {
+    element.classList.remove(`.selected-circle`);
+  });
+
+  document
+    .querySelectorAll(`.nav-dot`)
+    [[...document.querySelectorAll(`h1, h2`)].indexOf(titles[0])].classList.add(
+      `.selected-circle`
+    );
+};
+
+updateList();
+
+window.addEventListener(`scroll`, () => {
+  updateList();
+});
